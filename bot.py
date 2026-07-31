f2edeb05a6bd4d5e3003990e46a5a4afb775541e
jesuspalaciosc2026-spec <jesuspalaciosc2026@gmail.com>
2026-07-31 17:42:59 -0500

    Update bot.py mejoras

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ PASSWORD = os.getenv("IQ_PASSWORD")
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-AMOUNT = 35
+AMOUNT = 5500
 PAIR = "EURUSD-OTC"
 
 trade_open = False
